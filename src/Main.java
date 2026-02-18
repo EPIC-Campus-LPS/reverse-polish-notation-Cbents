d1dc47e63311bb3f6dabca585456fe8f897d88d5
cadenbents <cbtech4581@gmail.com>
2026-02-18 15:06:02 -0700

    Notation day 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        String result = String.valueOf(ReversePolishNotation.evaluatePostfix("72+9/35*"));
+        System.out.println(result); //ONLY WORKS FOR SINGLE DIGIT NUMBERS RIGHT NOW
 
     }
 }
--- src/ReversePolishNotation.java
+++ src/ReversePolishNotation.java
@@ -1,48 +1,65 @@
 
 public class ReversePolishNotation {
 
-    public static int evaluatePostfix(String input){
+    public static int evaluatePostfix(String input) {
         Stack s = new Stack();
         String numFilter = "-+*/^";
         char[] inList = input.toCharArray();
+        int numCount = 0;
+        int operatorCount = 0;
 
-        for (char c : inList){
-            if (numFilter.indexOf(c) == -1){ // If c is actually a number
-                s.push(Character.toString(c));
-
+        //Check for invalid input first:
+        for (char c: inList){
+            if (numFilter.indexOf(c) == -1){
+                numCount ++;
             }
             else{
-                if (c == '-'){
+                operatorCount ++;
+            }
+        }
+        if ((operatorCount + 1) != numCount){
+            throw new IllegalArgumentException("Postfix string is invalid.");
+        }
+
+
+
+        for (char c : inList) {
+            if (numFilter.indexOf(c) == -1) { // If c is actually a number
+                s.push(Character.toString(c));
+
+            } else {
+                if (c == '-') {
                     int num2 = Integer.parseInt(s.pop());
                     int num1 = Integer.parseInt(s.pop());
                     s.push(Integer.toString(num1 - num2));
-                }
-                else if (c == '+'){
+                } else if (c == '+') {
                     int num2 = Integer.parseInt(s.pop());
                     int num1 = Integer.parseInt(s.pop());
                     s.push(Integer.toString(num1 + num2));
-                }
-                else if (c == '*'){
+                } else if (c == '*') {
                     int num2 = Integer.parseInt(s.pop());
                     int num1 = Integer.parseInt(s.pop());
                     s.push(Integer.toString(num1 * num2));
-                }
-                else if (c == '/'){
+                } else if (c == '/') {
                     int num2 = Integer.parseInt(s.pop());
                     int num1 = Integer.parseInt(s.pop());
                     s.push(Integer.toString(num1 / num2));
-                }
-                else if (c == '^'){
+                } else if (c == '^') {
                     int num2 = Integer.parseInt(s.pop());
                     int num1 = Integer.parseInt(s.pop());
-                    s.push(Integer.toString(Math.powExact(num1, num2)));
+                    s.push(Integer.toString((int) Math.pow(num1, num2)));
                 }
             }
         }
+        return Integer.parseInt(s.peek());
     }
 
-    public static String infixToPostfix(String input){
+    public static String infixToPostfix(String input) {
         Stack s = new Stack();
+
+        String name = "hello";
+        return name;
     }
 
 }
+
